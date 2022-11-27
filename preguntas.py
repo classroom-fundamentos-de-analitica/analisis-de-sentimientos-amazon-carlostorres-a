4c73af7bd71a46d589a9932ec6c0042e816b2b87
C4-T0 <carlostorresh1@gmail.com>
2022-11-27 18:53:04 -0500

    .

--- preguntas.py
+++ preguntas.py
@@ -197,11 +197,11 @@ def pregunta_06():
     gridSearchCV = pregunta_04()
 
     # Cargue los datos generados en la pregunta 01.
-    _, X_untagged, _, _ = pregunta_01()
+    x_tagged, y_tagged, x_untagged, y_untagged = pregunta_01()
 
     # pronostique la polaridad del sentimiento para los datos
     # no etiquetados
-    y_untagged_pred = gridSearchCV.predict(X_untagged)
+    y_untagged_pred = gridSearchCV.predict(x_untagged)
 
     # Retorne el vector de predicciones
     return y_untagged_pred
